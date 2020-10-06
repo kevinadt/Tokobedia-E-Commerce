4379d6e3e4cb650ad6015a92c9a190b2efb89a29
kevinadt <72208376+kevinadt@users.noreply.github.com>
2020-10-07 00:21:49 +0700

    Add files via upload

---
+++ Util/DBManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TokopediaMain.Util
+{
+    public class DBManager
+    {
+
+        static SqlConnection connection = null;
+
+        private DBManager()
+        {
+
+        }
+
+        public static SqlConnection GetInstance()
+        {
+            if(DBManager.connection == null)
+            {
+                string attachDbFileName = @"'C:\Users\Aditya\Desktop\TM PSD\tokobedia\TokopediaMain\TokopediaMain\App_Data\MyDatabase.mdf'";
+
+                DBManager.connection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFileName=" + attachDbFileName + ";integrated Security=true");
+            }
+
+            return DBManager.connection;
+
+        }
+
+
+    }
+}
---
+++ Util/PageUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace TokopediaMain.Util
+{
+    public static class PageUtililty
+    {
+        public static void ErrorMessage(Control control, string errorMessage){
+            System.Web.UI.ScriptManager.RegisterClientScriptBlock(control, control.GetType(),"alertMessage", "alert('" + errorMessage + "')", true);
+        }
+    }
+}
